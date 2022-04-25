deb52c9e2a0f6ec5df5651de1d548240aa42cce3
Pizatol <dev.belmonte@gmail.com>
2022-04-25 22:31:41 +0200

    mis en place des informations

--- components/Weather.js
+++ components/Weather.js
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 import css from '../styles/Meteo.module.scss'
 
 export default function Weather() {
 
-	const api_call = async => {
 
+	const [toggleBg, setToggleBg] = useState(true)
 
-
-		 fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+	const api = {
+		key : "6c02486c9bb245de76e2f70778a587ba",
+		base : "https://api.openweathermap.org/data/2.5/"
 	}
+
+	const dateBuilder = (e) => {
+
+		let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
+		let days = ["lundi" , "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+		let day = days[e.getDay()]
+		let date = e.getDate()
+		let month = months[e.getMonth()]
+		let year = e.getFullYear()
+
+		return `${day} ${date} ${month} ${year}`
+
+	}
+
+
+
+	const toggle = (e) => {
+		e.preventDefault()
+		setToggleBg(!toggleBg)
+		console.log(toggleBg);
+		
+	}
+
 
 
   return (
 
 
 
-	 <div>
+	 <div className={ toggleBg ? css.global_container :  css.global_container_cold}>
+			<div className={css.search_bar}>
+			<form>
+					<input type="text" placeholder='Search city' />
+					<button onClick={(e)=> toggle(e)}> Search</button>
+			</form>
+			</div>
 
+			<div className={css.informations}>
+				<div className={css.location}>Toulouse, FR</div>
+				<div className={css.date}>{dateBuilder(new Date())} </div>
+			</div>
+
+			<div className={css.weather_box}>
+			<div className={css.temp}> 15°c</div>
+				<div className={css.weather}> Sunny</div>
+			</div>
 	 </div>
   )
 }
